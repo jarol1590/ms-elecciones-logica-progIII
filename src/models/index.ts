e2b50857e0f760d285ba50b8f1bd0add7bd947dc
jarol1590 <jarol1590@gmail.com>
2022-09-07 19:13:24 -0500

    cracion de plancha, bloque-cargo, bloque-votante, cargo

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,3 +3,7 @@ export * from './tipo-documento.model';
 export * from './eleccion.model';
 export * from './bloque.model';
 export * from './votante.model';
+export * from './bloque-votante.model';
+export * from './cargo.model';
+export * from './bloque-cargo.model';
+export * from './plancha.model';
